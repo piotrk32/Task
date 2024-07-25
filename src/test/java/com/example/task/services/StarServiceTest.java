5ae7d03c0f280b6d5e6e67a13728c56b7e67e191
piotrk32 <piotrkepistyuwm@gmail.com>
2024-07-25 10:05:46 +0200

    basic tests added

--- src/test/java/com/example/task/services/StarServiceTest.java
+++ src/test/java/com/example/task/services/StarServiceTest.java
@@ -3,14 +3,20 @@ package com.example.task.services;
 import com.example.task.models.star.Star;
 import com.example.task.repositories.StarRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class StarServiceTest {
 
@@ -36,6 +42,46 @@ class StarServiceTest {
         star.setItemName(name);
         star.setDistance(distance);
         return star;
+    }
+
+    @Test
+    void getNumberOfStarsByDistancesTest() {
+        when(starRepository.findAll()).thenReturn(starList);
+
+        Map<Long, Integer> starsByDistance = starService.getNumberOfStarsByDistances();
+
+        assertEquals(5, starsByDistance.size());
+        assertEquals(2, starsByDistance.get(4L));
+        assertEquals(1, starsByDistance.get(6L));
+        assertEquals(1, starsByDistance.get(8L));
+        assertEquals(1, starsByDistance.get(25L));
+        assertEquals(1, starsByDistance.get(433L));
+    }
+
+    @Test
+    void findClosestStarsTest() {
+        when(starRepository.findAll()).thenReturn(starList);
+
+        List<Star> closestStars = starService.findClosestStars(3);
+
+        assertEquals(3, closestStars.size());
+        assertEquals("Mars", closestStars.get(0).getItemName());
+        assertEquals("Pluto", closestStars.get(1).getItemName());
+        assertEquals("Earth", closestStars.get(2).getItemName());
+    }
+    @Test
+    void getUniqueStarsTest() {
+        when(starRepository.findAll()).thenReturn(starList);
+
+        Collection<Star> uniqueStars = starService.getUniqueStars();
+
+        assertEquals(6, uniqueStars.size());
+        Set<String> starNames = new HashSet<>();
+        uniqueStars.forEach(star -> starNames.add(star.getItemName()));
+        assertTrue(starNames.contains("Earth"));
+        assertTrue(starNames.contains("Pluto"));
+        assertTrue(starNames.contains("Saturn"));
+        assertTrue(starNames.contains("Jupiter"));
     }
 
 
