c49fa6d28f52a0bfebc685460d9262481a777c46
piotrk32 <piotrkepistyuwm@gmail.com>
2024-07-25 10:59:53 +0200

    field name changed

--- src/main/java/com/example/task/models/star/Star.java
+++ src/main/java/com/example/task/models/star/Star.java
@@ -2,6 +2,7 @@ package com.example.task.models.star;
 
 
 import com.example.task.models.basic.BasicEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -22,9 +23,11 @@ import lombok.experimental.FieldDefaults;
 public class Star extends BasicEntity {
 
     @Column(name = "star_name")
+    @Schema(description = "Name of the star", example = "Proxima Centauri")
     String starName;
 
     @Column(name = "distance")
+    @Schema(description = "Distance of the star from the sun in light-years", example = "4.24")
     Long distance;
 
     public Star(String starName, Long distance) {
--- src/main/resources/db/migration/V1__Database_init.sql
+++ src/main/resources/db/migration/V1__Database_init.sql
@@ -1,8 +1,8 @@
-CREATE SEQUENCE IF NOT EXISTS public.id_seq AS BIGINT START WITH 1000;
+CREATE SEQUENCE IF NOT EXISTS id_seq AS BIGINT START WITH 1000;
 
 CREATE TABLE stars (
                        id BIGINT PRIMARY KEY NOT NULL DEFAULT nextval('id_seq'),
-                       item_name VARCHAR(255) NOT NULL,
+                       star_name VARCHAR(255) NOT NULL,
                        distance BIGINT NOT NULL,
                        created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
                        modified_at TIMESTAMP WITHOUT TIME ZONE
