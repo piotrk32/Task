12fe49b092d35f9b03453fdfecbaad35a2225b16
piotrk32 <piotrkepistyuwm@gmail.com>
2024-07-25 09:52:10 +0200

    methods added and updated

--- src/main/java/com/example/task/services/StarService.java
+++ src/main/java/com/example/task/services/StarService.java
@@ -1,21 +1,55 @@
 package com.example.task.services;
 
+import com.example.task.exceptions.EntityNotFoundException;
 import com.example.task.models.star.Star;
+import com.example.task.repositories.StarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class StarService {
 
-    public List<Star> findClosestStars(List<Star> stars, int size) {
+    private final StarRepository starRepository;
+
+    public Star getStarById(Long starId) {
+        Star star = starRepository.findById(starId)
+                .orElseThrow(() -> new EntityNotFoundException("Item", "No item found with id: " + starId));
+        return star;
+    }
+
+    public List<Star> findClosestStars(int size) {
+        List<Star> stars = starRepository.findAll();
         return stars.stream()
                 .sorted(Comparator.comparingLong(Star::getDistance))
                 .limit(size)
                 .collect(Collectors.toList());
+    }
+
+    public Map<Long, Integer> getNumberOfStarsByDistances() {
+        List<Star> stars = starRepository.findAll();
+        return stars.stream()
+                .collect(Collectors.groupingBy(
+                        Star::getDistance,
+                        TreeMap::new,
+                        Collectors.collectingAndThen(Collectors.toList(), List::size)
+                ));
+    }
+
+    public Collection<Star> getUniqueStars() {
+        List<Star> stars = starRepository.findAll();
+        Map<String, Star> uniqueStars = new HashMap<>();
+        for (Star star : stars) {
+            uniqueStars.putIfAbsent(star.getItemName(), star);
+        }
+        return uniqueStars.values();
     }
 }
