3b0113fef4fb76038a1f980dbee58e711b30927f
piotrk32 <piotrkepistyuwm@gmail.com>
2024-07-25 11:13:57 +0200

    controller endpoints added

--- src/main/java/com/example/task/controlles/StarController.java
+++ src/main/java/com/example/task/controlles/StarController.java
@@ -8,12 +8,19 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/stars")
@@ -36,5 +43,32 @@ public class StarController {
     public ResponseEntity<Star> createStar(@RequestBody Star star) {
         Star starResponse = starService.createStar(star);
         return new ResponseEntity<>(starResponse, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{starId}")
+    public ResponseEntity<Void> deleteStar(@PathVariable Long starId) {
+        starService.deleteStar(starId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @PutMapping("/{starId}")
+    public ResponseEntity<Star> updateStar(@PathVariable Long starId, @RequestBody Star starDetails) {
+        Star updatedStar = starService.updateStar(starId, starDetails);
+        return new ResponseEntity<>(updatedStar, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/closest")
+    public List<Star> findClosestStars(@RequestParam int size) {
+        return starService.findClosestStars(size);
+    }
+
+    @GetMapping("/distances")
+    public Map<Long, Integer> getNumberOfStarsByDistances() {
+        return starService.getNumberOfStarsByDistances();
+    }
+
+    @GetMapping("/unique")
+    public Collection<Star> getUniqueStars() {
+        return starService.getUniqueStars();
     }
 }
