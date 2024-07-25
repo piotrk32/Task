4c7612ef7b2000c50e1ef2c62426743acb5c28d8
piotrk32 <piotrkepistyuwm@gmail.com>
2024-07-25 10:20:11 +0200

    TaskApplication.java updated with annotations

--- src/main/java/com/example/task/TaskApplication.java
+++ src/main/java/com/example/task/TaskApplication.java
@@ -1,9 +1,14 @@
 package com.example.task;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Welcome in Star Application!!!", version = "1.0.0"))
+@EnableJpaAuditing
 public class TaskApplication {
 
     public static void main(String[] args) {
