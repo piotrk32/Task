758e052f2663ed32ff453b4f26e7b84ce2015cf1
piotrk32 <piotrkepistyuwm@gmail.com>
2024-07-25 09:36:32 +0200

    BasiicEntity added

---
+++ src/main/java/com/example/task/models/basic/BasicEntity.java
@@ -0,0 +1,37 @@
+package com.example.task.models.basic;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class BasicEntity {
+
+    private static final String SEQUENCE_NAME = "id_seq";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "base_generator")
+    @SequenceGenerator(name = "base_generator", sequenceName = SEQUENCE_NAME, allocationSize = 1)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
